b3a65a46cf8821ac5a5d355e7a057fc7de52648f
Sandeep Kumar <51978973+sandeepshakya2019@users.noreply.github.com>
2024-05-22 11:09:57 +0530

    Adding Frontend Files and Folder

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CssBaseline } from "@mui/material";
+import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <HelmetProvider>
+        <CssBaseline />
+        <div onContextMenu={(e) => e.preventDefault()}>
+          <App />
+        </div>
+      </HelmetProvider>
+    </Provider>
+  </React.StrictMode>
+);
